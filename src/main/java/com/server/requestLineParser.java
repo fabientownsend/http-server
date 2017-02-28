f12136b0cc7e4e99d12b75df224c902e7ad9b1ab
fabien townsend <fabientownsend@gmail.com>
2017-02-28 13:51:40 +0000

    Add request line parser

---
+++ src/main/java/com/server/requestLineParser.java
@@ -0,0 +1,50 @@
+package com.server;
+
+public class RequestLineParser {
+    private String httpVerb;
+    private String uri;
+    private String httpVersionNumber;
+
+    public void parse(String httpRequest) throws RequestLineFormatException {
+        String[] requestLines = httpRequest.split("\\s+");
+
+        if (requestLines.length != 3) {
+            throw new RequestLineFormatException();
+        }
+
+        this.httpVerb = requestLines[0];
+        this.uri = requestLines[1];
+        this.httpVersionNumber = requestLines[2];
+
+        httpVerbChecker();
+    }
+
+    private void httpVerbChecker() throws RequestLineFormatException {
+        for (HttpVerb verb : HttpVerb.values()) {
+            if (verb.name().equals(httpVerb)) {
+                return;
+            }
+        }
+        throw new RequestLineFormatException();
+    }
+
+    public String getHttpVerb() {
+        return httpVerb;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public String versionNumber() {
+        return httpVersionNumber;
+    }
+
+    enum HttpVerb {
+        DELETE, GET, HEAD, PUT;
+    }
+
+    class RequestLineFormatException extends Exception {
+
+    }
+}
---
+++ src/test/java/com/server/requestLineParserTest.java
@@ -0,0 +1,77 @@
+package com.server;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+import com.server.RequestLineParser.RequestLineFormatException;
+
+public class RequestLineParserTest {
+    @Test
+    public void returnsDeleteHttpVerb()  throws RequestLineFormatException {
+        RequestLineParser requestLineParser = new RequestLineParser();
+        requestLineParser.parse("DELETE / HTTP/1.1");
+        assertThat(requestLineParser.getHttpVerb()).isEqualTo("DELETE");
+    }
+
+    @Test
+    public void returnsGetHttpVerb()  throws RequestLineFormatException {
+        RequestLineParser requestLineParser = new RequestLineParser();
+        requestLineParser.parse("GET / HTTP/1.1");
+        assertThat(requestLineParser.getHttpVerb()).isEqualTo("GET");
+    }
+
+    @Test
+    public void returnsHeadHttpVerb()  throws RequestLineFormatException {
+        RequestLineParser requestLineParser = new RequestLineParser();
+        requestLineParser.parse("HEAD / HTTP/1.1");
+        assertThat(requestLineParser.getHttpVerb()).isEqualTo("HEAD");
+    }
+
+    @Test
+    public void returnsPutHttpVerb()  throws RequestLineFormatException {
+        RequestLineParser requestLineParser = new RequestLineParser();
+        requestLineParser.parse("PUT / HTTP/1.1");
+        assertThat(requestLineParser.getHttpVerb()).isEqualTo("PUT");
+    }
+
+    @Test
+    public void returnsTheUri()  throws RequestLineFormatException {
+        RequestLineParser requestLineParser = new RequestLineParser();
+        requestLineParser.parse("HEAD /logs HTTP/1.1");
+        assertThat(requestLineParser.getUri()).isEqualTo("/logs");
+    }
+
+    @Test
+    public void returnTheVersionNumber()  throws RequestLineFormatException {
+        RequestLineParser requestLineParser = new RequestLineParser();
+        requestLineParser.parse("HEAD / HTTP/1.1");
+        assertThat(requestLineParser.versionNumber()).isEqualTo("HTTP/1.1");
+    }
+
+    @Test
+    public void throwsAnExceptionWhenNumberOffArgumentsIsntThree() {
+        RequestLineParser requestLineParser = new RequestLineParser();
+
+        assertThatExceptionOfType(RequestLineFormatException.class).isThrownBy(
+                () -> requestLineParser.parse("")
+        );
+    }
+
+    @Test
+    public void throwsAnExceptionIfHttpVerbDoesntExist()  throws RequestLineFormatException {
+        RequestLineParser requestLineParser = new RequestLineParser();
+
+        assertThatExceptionOfType(RequestLineFormatException.class).isThrownBy(
+                () -> requestLineParser.parse("HELLO / HTTP/1.1")
+        );
+    }
+
+    @Test
+    public void throwsAnExceptionIfHttpVerbIsntUpperCase()  throws RequestLineFormatException {
+        RequestLineParser requestLineParser = new RequestLineParser();
+
+        assertThatExceptionOfType(RequestLineFormatException.class).isThrownBy(
+                () -> requestLineParser.parse("get / HTTP/1.1")
+        );
+    }
+}

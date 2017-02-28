4ddbe3162557b00b518d6202b5e43f4dcdd526bb
fabien townsend <fabientownsend@gmail.com>
2017-02-28 15:38:13 +0000

    Server reply to a get request

--- src/main/java/com/server/Server.java
+++ src/main/java/com/server/Server.java
@@ -7,15 +7,27 @@ import java.io.PrintWriter;
 public class Server {
     private BufferedReader input;
     private PrintWriter output;
+    private RequestLineParser requestLineParser;
+    private static final String ERROR_REQUEST = "error request";
 
     public Server(BufferedReader input, PrintWriter output) {
         this.input = input;
         this.output = output;
+        this.requestLineParser = new RequestLineParser();
     }
 
     public void run() {
         try {
-            output.println(input.readLine());
+            String httpRequest = input.readLine();
+
+            try {
+                requestLineParser.parse(httpRequest);
+                String response = requestLineParser.versionNumber() + " " + 200 + " OK";
+                output.println(response);
+            } catch (RequestLineParser.RequestLineFormatException e) {
+                output.println(ERROR_REQUEST);
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/test/java/com/server/ServerTest.java
+++ src/test/java/com/server/ServerTest.java
@@ -8,13 +8,24 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class ServerTest {
     @Test
-    public void returnTheInput() {
-        BufferedReader input = new BufferedReader(new StringReader("Coucou"));
+    public void returnErrorMessageWhenBadRequest() {
+        BufferedReader input = new BufferedReader(new StringReader("BAD request"));
         StringWriter out = new StringWriter();
         PrintWriter output = new PrintWriter(out, true);
         Server server = new Server(input, output);
         server.run();
 
-        assertThat(out.toString()).contains("Coucou");
+        assertThat(out.toString()).isEqualTo("error request\n");
+    }
+
+    @Test
+    public void returnTheRequestWhenCorrect() {
+        BufferedReader input = new BufferedReader(new StringReader("GET / HTTP/1.1"));
+        StringWriter out = new StringWriter();
+        PrintWriter output = new PrintWriter(out, true);
+        Server server = new Server(input, output);
+        server.run();
+
+        assertThat(out.toString()).isEqualTo("HTTP/1.1 200 OK\n");
     }
 }

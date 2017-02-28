ef6c4cff2431fde5b82fb8192d9030f46fc12f69
fabien townsend <fabientownsend@gmail.com>
2017-02-28 15:35:19 +0000

    Use the args parser to get serversettings

--- src/main/java/com/server/Main.java
+++ src/main/java/com/server/Main.java
@@ -8,11 +8,12 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-    static final int PORT = 5000;
-
     public static void main(String[] args) {
+        ServerSettingsParser serverSettingsParser = new ServerSettingsParser();
+        serverSettingsParser.parse(args);
+
         try {
-            ServerSocket serverSocket = new ServerSocket(PORT);
+            ServerSocket serverSocket = new ServerSocket(serverSettingsParser.getPort());
             Socket clientSocket = serverSocket.accept();
 
             BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));

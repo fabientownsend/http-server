2481ae6a7e17386001bf091fb46d6bbc80ec09a8
fabien townsend <fabientownsend@gmail.com>
2017-03-06 15:16:34 +0000

    Add http request provider

---
+++ src/main/java/com/server/HttpRequestProvider.java
@@ -0,0 +1,78 @@
+package com.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class HttpRequestProvider {
+    private final BufferedReader socketInput;
+    private final String contentLengthTitle = "Content-Length";
+    private final String emptyString = "";
+    private final String newLine = "\n";
+
+    public HttpRequestProvider(BufferedReader socketInput) {
+        this.socketInput = socketInput;
+    }
+
+    public String getRequest() throws IOException {
+        String httpRequest = emptyString;
+        httpRequest += getHeader();
+
+        if (hasBody(httpRequest)) {
+            httpRequest += getBody(bodyLength(httpRequest));
+        }
+
+        return httpRequest.trim();
+    }
+
+    private int bodyLength(String requestHeader) {
+        int contentLength = 0;
+        String[] headerSection = requestHeader.split("\\n");
+
+        for (String information : headerSection) {
+            String[] splitInformation = information.split(":", 2);
+            if (splitInformation[0].equals(contentLengthTitle)) {
+                contentLength = Integer.parseInt(splitInformation[1].trim());
+            }
+        }
+
+        return contentLength;
+    }
+
+    private boolean hasBody(String requestHeader) {
+        String[] headerSection = requestHeader.split("\\n");
+
+        for (String information : headerSection) {
+            String[] splitInformation = information.split(":", 2);
+            if (splitInformation[0].equals(contentLengthTitle)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private String getHeader() throws IOException {
+        String temp;
+        String requestHeader = socketInput.readLine() + newLine;
+
+        while ((temp = socketInput.readLine()) != null) {
+            if (temp.equals(emptyString)) {
+                break;
+            }
+
+            requestHeader += temp + newLine;
+        }
+
+        return requestHeader;
+    }
+
+    private String getBody(int length) throws IOException {
+        String requestBody = newLine;
+
+        for (int i = 0; i < length; i++) {
+            requestBody += (char)socketInput.read();
+        }
+
+        return requestBody;
+    }
+}
---
+++ src/test/java/com/server/HttpRequestProviderTest.java
@@ -0,0 +1,63 @@
+package com.server;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HttpRequestProviderTest {
+    @Test
+    public void returnsTheRequestHeader() throws  IOException {
+        String simpleHttpRequest = "GET / HTTP/1.1\n"
+            + "Host: localhost:5000\n"
+            + "Connection: Keep-Alive\n"
+            + "User-Agent: Apache-HttpClient/4.3.5 (java 1.5)\n"
+            + "Accept-Encoding: gzip,deflate";
+
+        BufferedReader socketInput = new BufferedReader(new StringReader(simpleHttpRequest));
+        HttpRequestProvider httpRequestProvider = new HttpRequestProvider(socketInput);
+
+        assertThat(httpRequestProvider.getRequest()).isEqualTo(simpleHttpRequest);
+    }
+
+    @Test
+    public void returnsTheRequestHeaderWithBody() throws  IOException {
+        String httpRequestWithBody = "POST /form HTTP/1.1\n"
+            + "Content-Length: 11\n"
+            + "Host: localhost:5000\n"
+            + "Connection: Keep-Alive\n"
+            + "User-Agent: Apache-HttpClient/4.3.5 (java 1.5)\n"
+            + "Accept-Encoding: gzip,deflate\n"
+            + "\n"
+            + "data=fatcat";
+        BufferedReader socketInput = new BufferedReader(new StringReader(httpRequestWithBody));
+        HttpRequestProvider httpRequestProvider = new HttpRequestProvider(socketInput);
+
+        assertThat(httpRequestProvider.getRequest()).isEqualTo(httpRequestWithBody);
+    }
+
+    @Test
+    public void doesntReturnBodyIfContentLengthIsntPresent() throws  IOException {
+        String httpRequestWithBody = "POST /form HTTP/1.1\n"
+            + "Host: localhost:5000\n"
+            + "Connection: Keep-Alive\n"
+            + "User-Agent: Apache-HttpClient/4.3.5 (java 1.5)\n"
+            + "Accept-Encoding: gzip,deflate\n"
+            + "\n"
+            + "data=fatcat";
+
+        BufferedReader socketInput = new BufferedReader(new StringReader(httpRequestWithBody));
+        HttpRequestProvider httpRequestProvider = new HttpRequestProvider(socketInput);
+
+        assertThat(httpRequestProvider.getRequest()).isEqualTo(
+            "POST /form HTTP/1.1\n"
+            + "Host: localhost:5000\n"
+            + "Connection: Keep-Alive\n"
+            + "User-Agent: Apache-HttpClient/4.3.5 (java 1.5)\n"
+            + "Accept-Encoding: gzip,deflate"
+        );
+    }
+}

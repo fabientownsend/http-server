2c292ac91675e14eeb999fd3a879c173036d0d3e
fabien townsend <fabientownsend@gmail.com>
2017-03-14 11:20:24 +0000

    Create a thread for each socket

--- src/main/java/com/server/Main.java
+++ src/main/java/com/server/Main.java
@@ -7,6 +7,8 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.LinkedList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,27 +21,27 @@ public class Main {
         FileHandler fileHandle = new FileHandler("logs/logger.log", false);
         fileHandle.setFormatter(new SimpleFormatter());
         LOGGER.addHandler(fileHandle);
-        Socket clientSocket;
 
         ServerSettingsParser serverSettingsParser = new ServerSettingsParser();
 
         ServerSocket serverSocket = new ServerSocket(serverSettingsParser.getPort());
         LinkedList<String> memory = new LinkedList<>();
+        ExecutorService executor = Executors.newFixedThreadPool(100);
 
         while (true) {
-            try {
-                clientSocket = serverSocket.accept();
-                BufferedReader input = new BufferedReader(
-                    new InputStreamReader(clientSocket.getInputStream()));
-                OutputStream output = clientSocket.getOutputStream();
-
-                Server server = new Server(input, output, memory, serverSettingsParser.getDirectory());
-                server.start();
-                clientSocket.close();
-            } catch (Exception exception) {
-                LOGGER.log(Level.WARNING, exception.getMessage());
-                System.out.println(exception.getMessage());
-            }
+            final Socket clientSocket = serverSocket.accept();
+            executor.submit(() -> {
+                try {
+                    BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    OutputStream output = clientSocket.getOutputStream();
+                    Server server = new Server(input, output, memory, serverSettingsParser.getDirectory());
+                    server.start();
+                    clientSocket.close();
+                } catch (Exception exception) {
+                    LOGGER.log(Level.WARNING, exception.getMessage());
+                    System.out.println(exception.getMessage());
+                }
+            });
         }
     }
 }

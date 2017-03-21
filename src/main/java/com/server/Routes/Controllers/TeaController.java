5c7bbd0da6bd80c211a02cd8522b5bef0718fcf4
fabien townsend <fabientownsend@gmail.com>
2017-03-21 08:35:03 +0000

    Return bad request when it is not a get

--- src/main/java/com/server/Routes/Controllers/TeaController.java
+++ src/main/java/com/server/Routes/Controllers/TeaController.java
@@ -3,12 +3,18 @@ package com.server.Routes.Controllers;
 import com.server.HttpHeaders.HttpStatusCode;
 import com.server.HttpRequest.ClientHttpRequest;
 import com.server.HttpResponse.HttpResponse;
+import com.server.HttpVerb;
 
 public class TeaController implements BaseController {
     public HttpResponse execute(ClientHttpRequest clientHttpRequest) {
         HttpResponse httpResponse = new HttpResponse(clientHttpRequest.getHttpVersion());
-        httpResponse.statusCode(HttpStatusCode.OK);
-        httpResponse.addHeader("Content-Type", "text/html");
+
+        if (clientHttpRequest.getVerb() == HttpVerb.GET) {
+            httpResponse.statusCode(HttpStatusCode.OK);
+        } else {
+            httpResponse.statusCode(HttpStatusCode.METHOD_NOT_ALLOWED);
+        }
+
         return httpResponse;
     }
 }
--- src/test/java/com/server/Routes/Controllers/TeaControllerTest.java
+++ src/test/java/com/server/Routes/Controllers/TeaControllerTest.java
@@ -8,12 +8,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class TeaControllerTest {
     @Test
-    public void returnTeaRedirection() {
+    public void returnOkWhenGetRequest() {
         ClientHttpRequest clientHttpRequest = new ClientHttpRequest();
         clientHttpRequest.setVerb(HttpVerb.GET);
         clientHttpRequest.setHttpVersion("HTTP/1.1");
 
         TeaController teaController = new TeaController();
-        assertThat(teaController.execute(clientHttpRequest).response()).contains("HTTP/1.1 200 OK".getBytes());
+        String httpResponse = new String(teaController.execute(clientHttpRequest).response());
+        assertThat(httpResponse).isEqualTo("HTTP/1.1 200 OK");
+    }
+
+    @Test
+    public void returnsMethodNotAllowedForOtherRequests() {
+        ClientHttpRequest clientHttpRequest = new ClientHttpRequest();
+        TeaController teaController = new TeaController();
+        String httpResponse;
+
+        for (HttpVerb verb : HttpVerb.values()) {
+            if (verb != HttpVerb.GET) {
+                clientHttpRequest.setVerb(verb);
+                clientHttpRequest.setHttpVersion("HTTP/1.1");
+
+                httpResponse = new String(teaController.execute(clientHttpRequest).response());
+                assertThat(httpResponse).isEqualTo("HTTP/1.1 405 Method Not Allowed");
+            }
+        }
     }
 }

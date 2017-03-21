bcdb53d7bc4ecfd53c510f4afaf7e2c8308f8cc9
fabien townsend <fabientownsend@gmail.com>
2017-03-21 18:46:52 +0000

    Move verb dispatcher into controller interface

--- src/main/java/com/server/Routes/Controllers/BaseController.java
+++ src/main/java/com/server/Routes/Controllers/BaseController.java
@@ -3,45 +3,62 @@ package com.server.Routes.Controllers;
 import com.server.HttpHeaders.HttpStatusCode;
 import com.server.HttpRequest.ClientHttpRequest;
 import com.server.HttpResponse.HttpResponse;
+import com.server.HttpVerb;
+
+import static com.server.HttpVerb.*;
 
 public interface BaseController {
+    default HttpResponse execute(ClientHttpRequest clientHttpRequest) {
+        HttpVerb verb = clientHttpRequest.getVerb();
+
+        if (verb == GET) {
+            return doGet(clientHttpRequest);
+        } else if (verb == POST) {
+            return doPost(clientHttpRequest);
+        } else if (verb == PUT) {
+            return doPut(clientHttpRequest);
+        } else if (verb == DELETE) {
+            return doDelete(clientHttpRequest);
+        } else if (verb == OPTIONS) {
+            return doOptions(clientHttpRequest);
+        } else if (verb == PATCH) {
+            return doPatch(clientHttpRequest);
+        } else if (verb == HEAD) {
+            return doHead(clientHttpRequest);
+        } else {
+            return methodNotAllowed();
+        }
+    }
+
     default HttpResponse doGet(ClientHttpRequest clientHttpRequest) {
-        HttpResponse httpResponse = new HttpResponse();
-        httpResponse.statusCode(HttpStatusCode.METHOD_NOT_ALLOWED);
-        return httpResponse;
+        return methodNotAllowed();
     }
 
     default HttpResponse doPost(ClientHttpRequest clientHttpRequest) {
-        HttpResponse httpResponse = new HttpResponse();
-        httpResponse.statusCode(HttpStatusCode.METHOD_NOT_ALLOWED);
-        return httpResponse;
+        return methodNotAllowed();
     }
 
     default HttpResponse doPut(ClientHttpRequest clientHttpRequest) {
-        HttpResponse httpResponse = new HttpResponse();
-        httpResponse.statusCode(HttpStatusCode.METHOD_NOT_ALLOWED);
-        return httpResponse;
+        return methodNotAllowed();
     }
 
     default HttpResponse doDelete(ClientHttpRequest clientHttpRequest) {
-        HttpResponse httpResponse = new HttpResponse();
-        httpResponse.statusCode(HttpStatusCode.METHOD_NOT_ALLOWED);
-        return httpResponse;
+        return methodNotAllowed();
     }
 
     default HttpResponse  doOptions(ClientHttpRequest clientHttpRequest) {
-        HttpResponse httpResponse = new HttpResponse();
-        httpResponse.statusCode(HttpStatusCode.METHOD_NOT_ALLOWED);
-        return httpResponse;
+        return methodNotAllowed();
     }
 
     default HttpResponse doPatch(ClientHttpRequest clientHttpRequest) {
-        HttpResponse httpResponse = new HttpResponse();
-        httpResponse.statusCode(HttpStatusCode.METHOD_NOT_ALLOWED);
-        return httpResponse;
+        return methodNotAllowed();
     }
 
     default HttpResponse doHead(ClientHttpRequest clientHttpRequest) {
+        return methodNotAllowed();
+    }
+
+    default HttpResponse methodNotAllowed() {
         HttpResponse httpResponse = new HttpResponse();
         httpResponse.statusCode(HttpStatusCode.METHOD_NOT_ALLOWED);
         return httpResponse;
--- src/main/java/com/server/Routes/RequestController.java
+++ src/main/java/com/server/Routes/RequestController.java
@@ -3,12 +3,10 @@ package com.server.Routes;
 import com.server.HttpHeaders.HttpStatusCode;
 import com.server.HttpRequest.ClientHttpRequest;
 import com.server.HttpResponse.HttpResponse;
-import com.server.HttpVerb;
 import com.server.Routes.Controllers.BaseController;
 
 import java.util.logging.Level;
 
-import static com.server.HttpVerb.*;
 import static com.server.Main.LOGGER;
 
 public class RequestController {
@@ -24,7 +22,7 @@ public class RequestController {
         BaseController route = router.route(clientHttpRequest.getUri());
 
         try {
-            httpResponse = executeVerb(clientHttpRequest, route);
+            httpResponse = route.execute(clientHttpRequest);
         } catch (Exception e) {
             httpResponse.statusCode(HttpStatusCode.INTERNAL_SERVER_ERROR);
             httpResponse.content("The server encountered an unexpected condition");
@@ -32,28 +30,5 @@ public class RequestController {
         }
 
         return  httpResponse;
-    }
-
-    private HttpResponse executeVerb(ClientHttpRequest clientHttpRequest, BaseController route) {
-        HttpVerb currentVerb = clientHttpRequest.getVerb();
-        if (currentVerb == GET) {
-            return route.doGet(clientHttpRequest);
-        } else if (currentVerb == POST) {
-            return route.doPost(clientHttpRequest);
-        } else if (currentVerb == PUT) {
-            return route.doPut(clientHttpRequest);
-        } else if (currentVerb == DELETE) {
-            return route.doDelete(clientHttpRequest);
-        } else if (currentVerb == OPTIONS) {
-            return route.doOptions(clientHttpRequest);
-        } else if (currentVerb == PATCH) {
-            return route.doPatch(clientHttpRequest);
-        } else if (currentVerb == HEAD) {
-            return route.doHead(clientHttpRequest);
-        } else {
-            HttpResponse httpResponse = new HttpResponse(clientHttpRequest.getHttpVersion());
-            httpResponse.statusCode(HttpStatusCode.METHOD_NOT_ALLOWED);
-            return httpResponse;
-        }
     }
 }

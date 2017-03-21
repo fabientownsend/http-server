e54b65deff89a5c34fcc1242dc34011ead98418d
fabien townsend <fabientownsend@gmail.com>
2017-03-21 08:44:53 +0000

    Add default http response when verb not allowed

--- src/main/java/com/server/Routes/Controllers/RedirectionController.java
+++ src/main/java/com/server/Routes/Controllers/RedirectionController.java
@@ -3,12 +3,19 @@ package com.server.Routes.Controllers;
 import com.server.HttpHeaders.HttpStatusCode;
 import com.server.HttpRequest.ClientHttpRequest;
 import com.server.HttpResponse.HttpResponse;
+import com.server.HttpVerb;
 
 public class RedirectionController implements BaseController {
     public HttpResponse execute(ClientHttpRequest clientHttpRequest) {
         HttpResponse httpResponse = new HttpResponse(clientHttpRequest.getHttpVersion());
-        httpResponse.statusCode(HttpStatusCode.OBJECT_MOVED);
-        httpResponse.addHeader("Location", "http://localhost:5000/");
+
+        if (clientHttpRequest.getVerb() == HttpVerb.GET) {
+            httpResponse.statusCode(HttpStatusCode.OBJECT_MOVED);
+            httpResponse.addHeader("Location", "http://localhost:5000/");
+        } else {
+            httpResponse.statusCode(HttpStatusCode.METHOD_NOT_ALLOWED);
+        }
+
         return httpResponse;
     }
 }
